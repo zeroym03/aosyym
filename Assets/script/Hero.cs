e05533df68f7dd72cf4fc2fb2262c18b5b6a6166
DESKTOP-8VJVGPD\admin <ym20031223@gmail.com>
2023-02-02 17:36:51 +0900

    게임오버 UI추가

--- Assets/script/Hero.cs
+++ Assets/script/Hero.cs
@@ -1,56 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.UIElements;
+
 enum heromove
 {
     Idle,
     move,//우킬릭
     w,//좌클릭
-    a,
-    s,
-    d,
+    die,
+
 
 
 }
 public class Hero : MonoBehaviour
 {
-
+    Color heroColor;
     [SerializeField] int _attack;
+    [SerializeField] float speed;
+    [SerializeField] int _hp;
     [SerializeField] Animator _ani;
-    [SerializeField] float  speed;
     [SerializeField] GameObject _rotate;
-
-    // Start is called before the first frame update
+    [SerializeField] GameObject _hero;
+    [SerializeField] GameObject _uiPanel;
+    [SerializeField] SkinnedMeshRenderer _render;
+    float _timer = 0f;
+    bool _hit = false;
+   // bool _isGameOver = false;
+    bool _move = true;
+  
     void Start()
     {
-        //_ani = GetComponent<Animator>();
         Debug.Log("확인");
+        heroColor = _render.material.color;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        move();
-        
+        if (_move == true)
+        {
+            move();
+        }
+        Hitted();
+        HittedColer();
     }
     public void move()
     {
-     
-        Vector3 v3 = Vector3.zero;
-        
+        Vector3 v3 = Vector3.zero;   
         if (Input.GetKey(KeyCode.W))
         {
             v3 += (Vector3.forward).normalized * Time.deltaTime * speed;
             _ani.SetInteger("hero", (int)heromove.move);
             _rotate.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            
         }
         if (Input.GetKey(KeyCode.A))
         {
             v3 += (Vector3.left).normalized * Time.deltaTime * speed;
             _ani.SetInteger("hero", (int)heromove.move);
             _rotate.transform.localRotation = Quaternion.Euler(new Vector3(0, 270, 0));
-
         }
         if (Input.GetKey(KeyCode.S))
         {
@@ -64,17 +71,50 @@ public class Hero : MonoBehaviour
             _ani.SetInteger("hero", (int)heromove.move);
             _rotate.transform.localRotation = Quaternion.Euler(new Vector3(0, 90, 0));
         }
-
         if (v3!= Vector3.zero)
         {
             transform.Translate(v3);
         }
-
         else
         {
             _ani.SetInteger("hero", (int)heromove.Idle);
-       
         }
+    }
+    public void Hitted()
+    {
+        if (Input.GetKeyDown(KeyCode.M))//공격받았을때
+        {
+            _hp -= 20;
+            _hit = true;
+        }
+        if (_hp <= 0)
+        {
+            Die();
+            _move = false;
+        }
+    }
+    public void HittedColer()
+    {
+        if (_hit == true)
+        {
+            _timer += Time.deltaTime;
+            _render.material.color = Color.red;
+            if (_timer > 0.5f)
+            {
+                _render.material.color = heroColor;
+                _timer = 0f;
+                _hit = false;
+            }
+        }
+    }
+    public void Die()
+    {
+        _ani.SetInteger("hero", (int)heromove.die);
+        GameOver();
+    }
+    public void GameOver()
+    {
+        _uiPanel.SetActive(true);
     }
 }
 
--- Assets/script/Legfox.cs
+++ Assets/script/Legfox.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UIElements;
+enum fox
+{
+    move,
+    attack,
+    die,
+}
 public class Legfox : MonoBehaviour
 {
-    Animator _ani;
-    float _hp;
-    float _speed;
-
-       
-    // Start is called before the first frame update
+    [SerializeField] Transform _hero;
+    [SerializeField] GameObject _rotate;
+    [SerializeField] float _speed;
+    [SerializeField] float _hp;
+    [SerializeField] int _attack;
+    [SerializeField]  Animator _ani;
+    Vector3 v3 = Vector3.zero;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // move();
+        attack();
+    }
+   public void move()
+    {
+        v3 += (Vector3.forward).normalized * Time.deltaTime * _speed;
+        _ani.SetInteger("legfox", (int)fox.move);
+        _rotate.transform.localRotation = Quaternion.Euler(new Vector3(90, 0, 0));
+        transform.Translate((_hero.position - _rotate.transform.position).normalized * Time.deltaTime * _speed);
+    }
+    public void attack()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            _ani.SetInteger("legfox", (int)fox.attack);
+        }
     }
 }
